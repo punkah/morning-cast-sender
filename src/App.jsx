d12346fdee47e125a28a1d11a6efac597f55ae66
punkah <orsolya.belasitz@irisium.com>
2019-07-14 20:45:19 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
-    componentDidMount() {
-    window["__onGCastApiAvailable"] = isAvailable => {
+  componentDidMount() {
+    /* eslint-disable */
+    window.__onGCastApiAvailable = isAvailable => {
       if (isAvailable) {
         this.initializeCastApi();
       }
@@ -13,41 +14,45 @@ class App extends Component {
   initializeCastApi = () => {
     const context = cast.framework.CastContext.getInstance();
     context.setOptions({
-      receiverApplicationId: "804329DF",
-      autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+      receiverApplicationId: '804329DF',
+      autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
     });
-    context.addEventListener(
-      cast.framework.CastContextEventType.CAST_STATE_CHANGED,
-      event => console.log(event)
+    context.addEventListener(cast.framework.CastContextEventType.CAST_STATE_CHANGED, event =>
+      console.log(event),
     );
-    context.addEventListener(
-      cast.framework.CastContextEventType.SESSION_STATE_CHANGED,
-      event => console.log(event)
+    context.addEventListener(cast.framework.CastContextEventType.SESSION_STATE_CHANGED, event =>
+      console.log(event),
     );
   };
 
   onClick = () => {
     const mediaInfo = new chrome.cast.media.MediaInfo(
-      "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4",
-      "video/mp4"
+      'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+      'video/mp4',
     );
     const request = new chrome.cast.media.LoadRequest(mediaInfo);
     const castSession = cast.framework.CastContext.getInstance().getCurrentSession();
     castSession.loadMedia(request).then(
-      function() {
-        console.log("Load succeed");
+      () => {
+        console.log('Load succeed');
       },
-      function(errorCode) {
-        console.log("Error code: " + errorCode);
-      }
+      errorCode => {
+        console.log(`Error code: ${errorCode}`);
+      },
     );
   };
 
   render() {
     return (
       <div className="app">
-        <google-cast-launcher />
-        <button onClick={this.onClick}>Cast</button>
+        <div id="app-cast-launcher">
+          <google-cast-launcher />
+        </div>
+        <div id="app-cast-button">
+          <button onClick={this.onClick} type="button">
+            Cast
+          </button>
+        </div>
       </div>
     );
   }
